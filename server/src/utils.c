b15151d9b2016b32406c994efaf521c8d6c99899
tomibarra <tomi.barrientos.tb97@gmail.com>
2024-08-21 05:03:48 +0000

    Agrego server

--- server/src/utils.c
+++ server/src/utils.c
@@ -4,8 +4,7 @@ t_log* logger;
 
 int iniciar_servidor(void)
 {
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// Quitar esta línea cuando hayamos terminado de implementar la funcion. QUITADA
 
 	int socket_servidor;
 
@@ -19,10 +18,11 @@ int iniciar_servidor(void)
 	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
 
 	// Creamos el socket de escucha del servidor
-
+	socket_servidor = socket(servinfo->ai_family,servinfo->ai_socktype,servinfo->ai_protocol);
 	// Asociamos el socket a un puerto
-
+	bind(socket_servidor,servinfo->ai_addr,servinfo->ai_addrlen);
 	// Escuchamos las conexiones entrantes
+	listen(socket_servidor, SOMAXCONN);
 
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
@@ -32,18 +32,18 @@ int iniciar_servidor(void)
 
 int esperar_cliente(int socket_servidor)
 {
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// Quitar esta línea cuando hayamos terminado de implementar la funcion. QUITADA
 
 	// Aceptamos un nuevo cliente
 	int socket_cliente;
+	socket_cliente = accept(socket_servidor,NULL,NULL);
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
 }
 
 int recibir_operacion(int socket_cliente)
-{
+{setsockopt(socket_cliente, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int));
 	int cod_op;
 	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
 		return cod_op;
